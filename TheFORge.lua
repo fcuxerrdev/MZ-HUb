61d8cb9d0ebb781908096ae029c55d8f56be6894
fcuxerrdev <fcuxerrdev@gmail.com>
2025-12-24 16:08:25 +0100

    Update TheFORge.lua

--- TheFORge.lua
+++ TheFORge.lua
@@ -109,9 +109,9 @@ Button.MouseLeave:Connect(function()
         { Size = originalSize }):Play()
 end)
 
-local Library = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/angeryy-tvy/Fluent-Port-Vxeze/refs/heads/main/Fluent.luau"))()
-local SaveManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/angeryy-tvy/Fluent-Port-Vxeze/refs/heads/main/Addons/SaveManager.luau"))()
-local InterfaceManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/angeryy-tvy/Fluent-Port-Vxeze/refs/heads/main/Addons/InterfaceManager.luau"))()
+local Library = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/fcuxerrdev/MZ-HUb/refs/heads/main/Fluent.luau"))()
+local SaveManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/fcuxerrdev/MZ-HUb/refs/heads/main/SaveManager.luau"))()
+local InterfaceManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/fcuxerrdev/MZ-HUb/refs/heads/main/InterfaceManager.luau"))()
 
 local isMobile = UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled
 local windowSize = isMobile and UDim2.fromOffset(1080, 860) or UDim2.fromOffset(580, 480)
